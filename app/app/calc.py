6336f6b69aeb96a4b0d15e4d929ea7b7a0523553
OgutuJoseph <onyango.joseph@yahoo.com>
2024-01-30 20:09:40 +0300

    Section 7: Write a test using TDD

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,5 +3,9 @@ Calcuulcator functions
 """
 
 def add(x, y):
-    """Add x & y and return result """
+    """Add x & y and return result"""
     return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return result"""
+    return y - x
