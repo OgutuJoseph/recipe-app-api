30dc573aafd1ee8aeb8cb3167e13505b7ecea294
OgutuJoseph <onyango.joseph@yahoo.com>
2024-02-29 09:45:44 +0300

    Section 17: Implement tag and ingredient filtering

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -105,7 +105,7 @@ class PrivateTagsApiTests(TestCase):
             price=Decimal('2.50'),
             user=self.user,
         )
-        recipe.ingredients.add(tag1)
+        recipe.tags.add(tag1)
 
         res = self.client.get(TAGS_URL, {'assigned_only': 1})
 
